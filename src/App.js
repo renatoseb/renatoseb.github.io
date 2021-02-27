b5dbe4d799c4be30feb7d1d288bb1c9bcad3e7eb
renatoseb <renatorodriguezllanos@gmail.com>
2021-02-26 23:52:04 -0500

    create read project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () =>{
+  return(
+    <header>lol</header>
+  )
+}
+
+render(<App />, document.getElementById("root"));
